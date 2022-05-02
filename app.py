a8c9511f13b6b3c8cdece4d0b9bfa32ced25a9e7
jennigaetan <gaetan.jenni@gmail.com>
2022-05-02 18:38:09 +0200

    Revert "v0"
    This reverts commit ed7b873362abe9323302333b80b46f82fa74ebf0.

--- app.py
+++ app.py
@@ -1,7 +1,5 @@
-from sqlalchemy import null
 from base import *
 from database import *
-import requests
 
 @app.route('/', methods = ['POST', 'GET'])
 def Add_sensor():
@@ -9,9 +7,16 @@ def Add_sensor():
         id_recover = int(request.form['add_id'])
         lat_recover = float(request.form['add_lat'])
         long_recover = float(request.form['add_long'])
-        request_data = {'id':str(id_recover),'long=':str(long_recover),'lat=':str(lat_recover)}
-        request_send = requests.post('http://localhost:5000/data', data=request_data)
-        return redirect('/')
+        #Error management
+        new_sensor = sensor(id = id_recover, lat = lat_recover, long = long_recover)
+        try:
+            db.session.add(new_sensor)
+            db.session.commit()
+            return redirect('/')
+        except:
+            flash("Retry")
+            print("error")
+            return redirect('/')        
     else:
         return render_template('add_sensor.html')
 
@@ -35,25 +40,7 @@ def Table():
         list_sensor = sensor.query.order_by(sensor.num).all()
         return render_template('table.html', list_sensor = list_sensor)
 
-#@app.route('/plot', methods = ['POST', 'GET'])
-#def Plot_sensor():
-
-@app.route('/data', methods = ['POST'])
-def Receive_data():
-    id_recover = int(request.args.get['id'])
-    lat_recover = float(request.args.get['lat'])
-    long_recover = float(request.args.get['long'])
-    new_sensor = sensor(id = id_recover, lat = lat_recover, long = long_recover)
-    try:
-        db.session.add(new_sensor)
-        db.session.commit()
-    except:
-        flash("Retry")
-        print("error")
-    return None
-
 
 if __name__ == "__main__":
-    # run app in debug mode on port 5000
-    app.run(debug=True, port=5000)              ###############################################
+    app.run()
     
