7aa28f5cbffe5854a89b554010cb4c0d5d0a85a6
jennigaetan <gaetan.jenni@gmail.com>
2022-05-18 12:50:59 +0200

    v

--- tools/add_data_v1.py
+++ tools/add_data_v1.py
@@ -1,12 +1,10 @@
-import sys
-#sys.path.append( './.' )
-#from database import data_sensor
 import datetime
 import requests
 import random
 
 
 link = 'http://pytagritech.isc.heia-fr.ch/data'
+
 
 DATA_MIN = -5
 DATA_MAX = 5
@@ -18,16 +16,11 @@ GAP = 0.01
 
 
 for n in range(0,N_DATA):
-    #new_data = data_sensor(
-    #   id = ID, 
-    #    time = datetime.datetime.today().isoformat(),
-    #    data = float(random.randrange(DATA_MIN*1000,DATA_MAX*1000,int(GAP*1000)))/1000)
 
     id = ID
     time = datetime.datetime.today().isoformat()
     data = float(random.randrange(DATA_MIN*1000,DATA_MAX*1000,int(GAP*1000)))/1000
 
     request_data = {'id':str(id),'time':str(time),'data':str(data)}
-    print(datetime.datetime.today().isoformat())
     response = requests.post(link, data=request_data)
-    print(response,datetime.datetime.today().isoformat())
+    print(response)
